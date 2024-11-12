45406e2c61c2ded38e68908541e342d440af8e89
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-11 21:26:48 -0300

    add performance and evento classes

--- src/main/java/br/com/TalentHub/entity/Evento.java
+++ src/main/java/br/com/TalentHub/entity/Evento.java
@@ -1,6 +1,5 @@
 package br.com.TalentHub.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,19 +15,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Artista {
+public class Evento {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     Long id;
 
-    @Column(
-        unique=true,
-        nullable=false
-    )
-    String email;
-
-    String estilo;
-    String ano_inicio; //para calcular os anos de experiência.
+    // incluindo datas, locais, e público-alvo
+    String data, local,publico_alvo;
 
     @OneToMany
     Performance performance;
--- src/main/java/br/com/TalentHub/entity/Performance.java
+++ src/main/java/br/com/TalentHub/entity/Performance.java
@@ -1,11 +1,10 @@
 package br.com.TalentHub.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,22 +13,22 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class Artista {
+@NoArgsConstructor
+public class Performance {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     Long id;
 
-    @Column(
-        unique=true,
-        nullable=false
-    )
-    String email;
+    // como duração, tipo de performance, cachê,
+    int duracao; //em minutos
+    String tipo, feedback;
+    Double cache; // em reais.
 
-    String estilo;
-    String ano_inicio; //para calcular os anos de experiência.
+    @ManyToOne
+    Artista artista;
 
-    @OneToMany
-    Performance performance;
+    @ManyToOne
+    Evento evento;
+
 }
